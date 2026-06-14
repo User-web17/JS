50e67645576fdfadc5ebae39f31b6732944d991e
ssh4rky <sigmovic1488@gmail.com>
2026-06-14 09:42:25 +0300

    new

--- Fifth/script4.js
+++ Fifth/script4.js
@@ -14,3 +14,38 @@ class SecurePrinter extends Printer {
 const myPrinter = new SecurePrinter();
 
 myPrinter.print("Тигр це вовк, а вовк це не тигр");
+
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+
+    eat() {
+        console.log(`${this.name} їсть`);
+    }
+}
+
+class Dog extends Animal {
+    constructor(name, breed) {
+        super(name);
+        this.breed = breed;
+    }
+
+    speak() {
+        console.log(`${this.name} гавкає!`);
+    }
+
+    executeCommand(command) {
+        console.log(`${this.name} executes command ${command}`);
+    }
+}
+
+class Cat extends Animal { }
+
+const myPet = new Dog('Тигр', 'Вівчарка');
+
+myPet.executeCommand('сидіти');
+myPet.eat();
+myPet.speak();
+
+console.log(myPet instanceof Animal); // true
